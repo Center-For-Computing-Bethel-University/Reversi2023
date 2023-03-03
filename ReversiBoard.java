b9aa44ae6099017aa96462d854fc875d2f1e7855
noah7000 <111531126+noah7000@users.noreply.github.com>
2023-03-03 02:10:22 -0500

    Add files via upload
    Completely reworked isValid and makeMove. This version is more streamline, being 72 LOC  shorter.

--- ReversiBoard.java
+++ ReversiBoard.java
@@ -7,21 +7,21 @@
   */
 public class ReversiBoard extends Board {
 	//Fields
+	private static final int ROWCHANGE = 0;
+    private static final int COLCHANGE = 1;
+    private static final int ROWBOUNDARY = 2;
+    private static final int COLBOUNDARY = 2;
+    private static final int NOBOUNDARY = -1;
+  
 	private ReversiPiece[][] pieces;
-	private boolean up, upR, right, downR, down, downL, left, upL;
+	private boolean[] dir;
 	
 	//Constructor
 	public ReversiBoard() {
 		size = DEFAULTSIZE;
         pieces = new ReversiPiece[size][size];
-        up = false;
-        upR = false;
-        right = false;
-        downR = false;
-        down = false;
-        downL = false;
-        left = false;
-        upL = false;
+        dir = new boolean[8];
+        for(int i=0; i<8;i++) dir[i] = false;
 		setGame();
 	}
 	
@@ -41,204 +41,132 @@ public class ReversiBoard extends Board {
 		getPiece(4,4).setType(2);
     }	
 	
+	public boolean onBoard(int x, int y) {
+        if (x> size-1 || x<0 || y> size-1 || y<0) return false;
+        return true;
+    }
+	
 	public void makeMove(int row, int col, ReversiPiece piece) {
-		int team, rowStep, colStep;
-		team = piece.getType();
+		int color, rowStep, colStep;
+		color = piece.getType();
 		rowStep = row;
 		colStep = col;
 		
-		if(isValid(row, col, piece)) {
+		int[] dirValues;
+		dirValues = new int[4];
+		
+		if (isValid(row, col, piece)) {
 			pieces[row][col] = piece;
-			//flip over the effected pieces
 			
-			if(up) { //Up direction
-				rowStep--;
-				while(getPiece(rowStep,col).getType() == piece.getOpp(team)) {
-					getPiece(rowStep,col).setType(team);
-					rowStep--;
+			for(int i=0; i<8;i++) {
+				dirValues = dirChange(i, dirValues);
+	
+				if(dir[i]) {
+					rowStep+= dirValues[ROWCHANGE];
+					colStep+= dirValues[COLCHANGE];
+					while(getPiece(rowStep,colStep).getType() == piece.getOpp(color)) { 
+						getPiece(rowStep,colStep).setType(color);
+						rowStep+= dirValues[ROWCHANGE];
+						colStep+= dirValues[COLCHANGE];
+					}
+					rowStep = row;
+					colStep = col;
 				}
-				rowStep = row;
-			}
-			//_______________________________________________________________
-			if(upR) { //Up-Right direction
-				rowStep--;
-				colStep++;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					getPiece(rowStep,colStep).setType(team);
-					rowStep--;
-					colStep++;
-				}
-				rowStep = row;
-				colStep = col;
-			}
-			//_______________________________________________________________
-			if(right) { //Right direction
-				colStep++;
-				while(getPiece(row,colStep).getType() == piece.getOpp(team)) { 
-					getPiece(row,colStep).setType(team);
-					colStep++;
-				}
-				colStep = col;
-			}
-			//_______________________________________________________________
-			if(downR) { //down-Right direction
-				rowStep++;
-				colStep++;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) {
-					getPiece(rowStep,colStep).setType(team);
-					rowStep++;
-					colStep++;
-				}
-				rowStep = row;
-				colStep = col;
-			}
-			//_______________________________________________________________
-			if(down) { //down direction
-				rowStep++;
-				while(getPiece(rowStep,col).getType() == piece.getOpp(team)) { 
-					getPiece(rowStep,col).setType(team);
-					rowStep++;
-				}
-				rowStep = row;
-			}
-			//_______________________________________________________________
-			if(downL) { //down-left direction
-				rowStep++;
-				colStep--;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					getPiece(rowStep,colStep).setType(team);
-					rowStep++;
-					colStep--;
-				}
-				rowStep = row;
-				colStep = col;
-			}
-			//_______________________________________________________________
-			if(left) { //left direction
-				colStep--;
-				while(getPiece(row,colStep).getType() == piece.getOpp(team)) { 
-					getPiece(row,colStep).setType(team);
-					colStep--;
-				}
-				colStep = col;
-			}
-			//_______________________________________________________________
-			if(upL) { //Up-left direction
-				rowStep--;
-				colStep--;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					getPiece(rowStep,colStep).setType(team);
-					rowStep--;
-					colStep--;
-				}
-				rowStep = row;
-				colStep = col;
 			}
 		}
 	}
 	
-	public boolean isValid(int row, int col, ReversiPiece piece) { 
-		int team, rowStep, colStep;
-		team = piece.getType();
+	public boolean isValid(int row, int col, ReversiPiece piece) {
+		int color, rowStep, colStep;
+		color = piece.getType();
 		rowStep = row;
 		colStep = col;
 		
-		if(row > size || col > size) return false;
+		int[] dirValues;
+		dirValues = new int[4];
+		
+		if(!onBoard(row, col)) return false; 			 //checks in the piece is off the board
 		if (pieces[row][col].getType() == Piece.BLANK) { //check for blank type
-			
-			if(row != 0) { //Up direction
-				rowStep--;
-				while(getPiece(rowStep,col).getType() == piece.getOpp(team)) { 
-					rowStep--;
+			for(int i=0; i<8;i++) {						 //For loop for each direction
+				dirValues = dirChange(i, dirValues);	 //Sets the direction values for the new direction
+	
+				if(row != dirValues[ROWBOUNDARY] || col != dirValues[COLBOUNDARY]) { //sets the boundaries so the steppers
+					rowStep+= dirValues[ROWCHANGE];									 //don't go <8 or >0
+					colStep+= dirValues[COLCHANGE];
+					while(getPiece(rowStep,colStep).getType() == piece.getOpp(color)) {  //Checks direction until there is an opp color
+						rowStep+= dirValues[ROWCHANGE];
+						colStep+= dirValues[COLCHANGE];
+					}
+					
+					if(getPiece(row + dirValues[ROWCHANGE], col + dirValues[COLCHANGE]).getType() != color) 
+						if(getPiece(rowStep,colStep).getType() == color) {dir[i] = true; return true;}
+					rowStep = row; //resets steppers for next iteration
+					colStep = col;
 				}
-				if(getPiece(row-1,col).getType() != team)
-					if(getPiece(rowStep,col).getType() == team) {up = true; return true;}
-				rowStep = row;
-			}
-			//_____________________________________________________________________________
-			if(row != 0 || col != size) { //Up-Right direction
-				rowStep--;
-				colStep++;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					rowStep--;
-					colStep++;
-				}
-				if(getPiece(row-1,col+1).getType() != team)
-					if(getPiece(rowStep,colStep).getType() == team) {upR = true; return true;}
-				rowStep = row;
-				colStep = col;
-			}
-			//_____________________________________________________________________________
-			if(col != size) { //Right direction
-				colStep++;
-				while(getPiece(row,colStep).getType() == piece.getOpp(team)) { 
-					colStep++;
-				}
-				if(getPiece(row,col+1).getType() != team)
-					if(getPiece(row,colStep).getType() == team) {right = true; return true;}
-				colStep = col;
-			}
-			//_____________________________________________________________________________
-			if(row != size || col != size) { //down-Right direction
-				rowStep++;
-				colStep++;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					rowStep++;
-					colStep++;
-				}
-				if(getPiece(row+1,col+1).getType() != team)
-					if(getPiece(rowStep,colStep).getType() == team) {downR = true; return true;}
-				rowStep = row;
-				colStep = col;
-			}
-			//_____________________________________________________________________________
-			if(row != size) { //down direction
-				rowStep++;
-				while(getPiece(rowStep,col).getType() == piece.getOpp(team)) { 
-					rowStep++;
-				}
-				if(getPiece(row+1,col).getType() != team)
-					if(getPiece(rowStep,col).getType() == team) {down = true; return true;}
-				rowStep = row;
-			}
-			//_____________________________________________________________________________
-			if(row != size || col != 0) { //down-left direction
-				rowStep++;
-				colStep--;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					rowStep++;
-					colStep--;
-				}
-				if(getPiece(row+1,col-1).getType() != team)
-					if(getPiece(rowStep,colStep).getType() == team) {downL = true; return true;}
-				rowStep = row;
-				colStep = col;
-			}
-			//_____________________________________________________________________________
-			if(col != 0) { //left direction
-				colStep--;
-				while(getPiece(row,colStep).getType() == piece.getOpp(team)) { 
-					colStep--;
-				}
-				if(getPiece(row,col-1).getType() != team)
-					if(getPiece(row,colStep).getType() == team) {left = true; return true;}
-				colStep = col;
-			}
-			//_____________________________________________________________________________
-			if(row != 0 || col != 0) { //Up-left direction
-				rowStep--;
-				colStep--;
-				while(getPiece(rowStep,colStep).getType() == piece.getOpp(team)) { 
-					rowStep--;
-					colStep--;
-				}
-				if(getPiece(row-1,col-1).getType() != team)
-					if(getPiece(rowStep,colStep).getType() == team) {upL = true; return true;}
-				rowStep = row;
-				colStep = col;
 			}
 		}
 		return false;
+	}
+	
+	//Provides relevant values for queried direction
+		//ROWCHANGE: The amount that the row changes for a given direction
+		//COLCHANGE: The amount that the column changes for a given direction
+		//ROWBOUNDARY: Shows the upper/lower row boundary of a given direction
+		//COLBOUNDARY: Shows the upper/lower column boundary of a given direction
+	public int[] dirChange(int dir, int[] dirValues) { 
+		for(int i=0; i<2;i++) dirValues[i] = 0;  		 	//Initializes direction changers
+		for(int i=2; i<4;i++) dirValues[i] = NOBOUNDARY; 	//Initializes direction boundaries
+		
+		switch (dir) {
+			case (0): 
+				dirValues[ROWCHANGE]=-1;
+				dirValues[ROWBOUNDARY]=0;
+				break;
+			
+			case (1): 
+				dirValues[ROWCHANGE]=-1; 
+				dirValues[COLCHANGE]=1;
+				dirValues[ROWBOUNDARY]=0;
+				dirValues[COLBOUNDARY]=size;
+				break;
+			
+			case (2): 
+				dirValues[COLCHANGE]=1;
+				dirValues[COLBOUNDARY]=size;
+				break;
+			
+			case (3): 
+				dirValues[ROWCHANGE]=1; 
+				dirValues[COLCHANGE]=1;
+				dirValues[ROWBOUNDARY]=size;
+				dirValues[COLBOUNDARY]=size;
+				break;
+			
+			case (4): 
+				dirValues[ROWCHANGE]=1;
+				dirValues[ROWBOUNDARY]=size;
+				break;
+			
+			case (5): 
+				dirValues[ROWCHANGE]=1; 
+				dirValues[COLCHANGE]=-1;
+				dirValues[ROWBOUNDARY]=size;
+				dirValues[COLBOUNDARY]=0;
+				break;
+			
+			case (6): 
+				dirValues[COLCHANGE]=-1;
+				dirValues[COLBOUNDARY]=0;
+				break;
+				
+			case (7): 
+				dirValues[ROWCHANGE]=-1; 
+				dirValues[COLCHANGE]=-1;
+				dirValues[ROWBOUNDARY]=0;
+				dirValues[COLBOUNDARY]=0;
+				break;
+		}
+		return dirValues;
 	}
 	
 	//Returns the score of a given player
