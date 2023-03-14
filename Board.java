5c4d93eea8f7324f74a8a72aba4aec494b3f2754
noah7000 <111531126+noah7000@users.noreply.github.com>
2023-03-14 10:18:08 -0400

    Revert "Patch 1"

--- Board.java
+++ Board.java
@@ -3,58 +3,95 @@
   *     be implemented in a subclass.
   *
   * @author      Cathy Bareiss
-  * @Modifier	 Noah Jackson
-  * @id          cathy.bareiss@betheluniversity.edu
+  * @id          cathy.bareiss@betheluniversity.edua
   * @course      CSC 321: Programming 3
   * @assignment  Reversi Project
   * @related     Piece
   */
-public abstract class Board implements BoardInterface{
-// Fields
-    protected Piece[][] pieces;
-    protected int size;
+public class Board {
+    // fields
+    /** pieces for the board */
+    private Piece[][] pieces;
+    /** size of the board */
+    private int size;  // not needed - can go by length but implemented instead
 
-//Class Constants
+    // public class constants about generic games
+    /** the default size of the board */
     public static final int DEFAULTSIZE = 8;
+    /** if the move specified indicates ending the game */
     public static final int ENDGAME = -1;
+    /** if the move specified was legal */
     public static final int LEGALMOVE = 0;
+    /** if the move specified was illegal */
     public static final int BADMOVE = 1;
-    
-//Constructors
+
+//Constructors and related methods
+    /** create a board of the default size */
     public Board() {
+        int row, column;
+
         size = DEFAULTSIZE;
         pieces = new Piece[size][size];
         blankBoard();
     }
 
-    //creates a square board of the size requested
+    /** create a square board of the size requested
+      * @param inSize - size of board
+      */
     public Board(int inSize) {
+        int row, column;
+
         size = inSize;
         pieces = new Piece[size][size];
         blankBoard();
     }
 
-    
-    // set all pieces to blank
-    protected abstract void blankBoard(); 
+    // sets entire board to blanks
+    /** set all pieces to blank */
+    private void blankBoard() {
+        int row, column;
+
+        for (row = 0; row < size; row++)
+            for (column = 0; column < size; column++)
+                pieces[row][column] = new Piece(Piece.BLANK);
+
+    }
 
 //Mutators
+    /** sets a given location to a specified piece
+      * @param inPiece the piece to store
+      * @param row the row to store the piece
+      * @param column the column to store the piece
+      */
     public void setPiece(Piece inPiece, int row, int column) {
         pieces[row][column] = inPiece;
     }
 
 //Accessors
+    /** returns the piece at a given location
+      * @param row the row of the piece to return
+      * @param column the column of the piece to return
+      * @return the piece requested
+      */
     public Piece getPiece(int row, int column) {
         return pieces[row][column];
     }
 
+    /** returns the size of the board
+      * @return the size of the board
+      */
     public int getSize() {
         return size;
     }
 
-//Helpers
+//Helper methods
 
     // is a given location on the board?
+    /** returns if the specified location is on the board
+      * @param row the row specifed
+      * @param column the column specified
+      * @return if the location is on the board
+      */
     public boolean onBoard(int row, int column) {
         if (row < 0) return false;
         if (column  < 0) return false;
@@ -63,7 +100,12 @@ public abstract class Board implements BoardInterface{
         return true;
     }
 
-    public boolean equals(BoardInterface otherBoard) {
+//Standard methods
+    /** returns if two boards are the same
+      * @param otherBoard the board to compare to
+      * @return if same pieces at every location
+      */
+    public boolean equals(Board otherBoard) {
         int row, column;
         boolean same;
 
@@ -77,6 +119,9 @@ public abstract class Board implements BoardInterface{
         return same;
     }
 
+    /** return string representing the board
+      * @return a string representing the board
+      */
     public String toString() {
         String tempString;
         int row, column;
--- Piece.java
+++ Piece.java
@@ -1,39 +1,54 @@
-
 /** This class will store a piece which is just an integer.
   * @author     Cathy Bareiss
-  * @Modifier	Noah Jackson
   * @id         cathy.bareiss@betheluniversity.edu
   * @course     CSIS 321:  Programming 3
   * @assignment Reversi Project
   * @related    none
  */
-public abstract class Piece {
-// fields
-    protected int type;
+public class Piece {
+    // fields
+    /** the type of piece */
+    private int type;
 
 //Class constants
+//   public
+    /** blank */
     public static final int BLANK = 0;
-    protected static final int MIN = 0;
-    protected static final int MAX = 2;
+//   private
+    /** smallest value allowed for a piece */
+    private static final int MIN = 0;
+    /** the largest value allowed for a piece */
+    private static final int MAX = 2;
 
 //Constructor
+    /** create a piece of the given type
+      * @param inType the type of piece to create
+      */
     public Piece (int inType) {
-        if (inType < MIN || inType > MAX) { // exception for invalid type needed
+
+        if (inType < MIN || inType > MAX) {
             System.err.println("invalid piece valid");
             type = -1;
         }
+        // exception for invalid type needed
         type = inType;
     }
-    
-//Accessors
+
+//Accessor
+    /** returns integer representing the type of piece
+      * @return the type of piece
+      */
     public int getType() {
         return type;
     }
-    
-//Modifier
-    protected abstract void setType(int newType); //Leaves setType to the subclass to implement
-		
+
 //Helper methods
+
+//Standard methods
+    /** if second piece is identical to current (deep compare)
+      * @param otherPiece piece to compare to it.
+      * @return if the to pieces are identical
+      */
     public boolean equals(Piece otherPiece) {
         if (type == otherPiece.getType()) return true;
         return false;
@@ -43,6 +58,9 @@ public abstract class Piece {
         return ' ';
     }
 
+    /** return a string representing a piece
+      * @return string for the type of piece
+      */
     public String toString() {
         if (type == BLANK) return "blank";
         if (type == -1) return "invalid piece";
